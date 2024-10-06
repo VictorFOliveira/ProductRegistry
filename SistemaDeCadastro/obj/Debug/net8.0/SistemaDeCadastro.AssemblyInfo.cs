81010cd2e89320617669f2a1e22bcfefde5f2410
Victor <victorfacundo122@hotmail.com>
2024-10-06 16:55:11 -0300

    Adicionando testes

--- SistemaDeCadastro/obj/Debug/net8.0/SistemaDeCadastro.AssemblyInfo.cs
+++ SistemaDeCadastro/obj/Debug/net8.0/SistemaDeCadastro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84bfc9f5618b4fa5729387b2e4c73aa0826292d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
